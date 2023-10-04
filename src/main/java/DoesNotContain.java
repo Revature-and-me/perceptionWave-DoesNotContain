e7caad5646580942c945123a94d63daf4dd66a97
Jericho Nasser <jerichonasser@yahoo.com>
2023-10-04 15:39:10 +0000

    JN

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,6 +15,10 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        boolean contained = true;
+        for (int i : arr){
+            if (target==i){contained=false;}
+        }
+        return contained;
     }
 }
